fd979864e28a3683144e8df4fbf727abb46e48b9
Akshay Shine Krishna <akshayshinekrishna@gmail.com>
2025-12-27 09:57:45 +0530

    initial commit

---
+++ src/test/java/com/trumio/task/aitools/AitoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trumio.task.aitools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
